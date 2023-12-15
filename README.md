62efecd28fd8d3dbaa5dac746725b1b19acc8727
tejababu846 <tejababu08@gmail.com>
2023-12-15 15:06:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+IF YOU WANT TO LOGIN LOGIN AS A NORMAL USER :
+USERNAME: raja
+PASSWORD: raja@2021
+
+IF YOU WANT TO LOGIN LOGIN AS A PRIME USER :
+USERNAME: rahul
+PASSWORD: rahul@2021
+
+# Sorting Products
+
+- Sorting Products
+  - Price High to Low
+  - Price Low to High
+- Installing Third-Party Packages
+  - react-icons
---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,145 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faLock, faLockOpen} from '@fortawesome/free-solid-svg-icons'
+import './index.css'
+
+class LoginForm extends Component {
+  state = {
+    username: '',
+    password: '',
+    showSubmitError: false,
+    errorMsg: '',
+    showPassword: false, // Step 1: Add state for password visibility
+  }
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  togglePasswordVisibility = () => {
+    // Step 2: Toggle password visibility state
+    this.setState(prevState => ({
+      showPassword: !prevState.showPassword,
+    }))
+  }
+
+  onSubmitSuccess = jwtToken => {
+    const {history} = this.props
+
+    Cookies.set('jwt_token', jwtToken, {
+      expires: 30,
+    })
+    history.replace('/')
+  }
+
+  onSubmitFailure = errorMsg => {
+    this.setState({showSubmitError: true, errorMsg})
+  }
+
+  submitForm = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const userDetails = {username, password}
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    if (response.ok === true) {
+      this.onSubmitSuccess(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+  }
+
+  renderPasswordField = () => {
+    const {password, showPassword} = this.state // Step 3: Get showPassword state
+    return (
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <div className="password-input-container">
+          <input
+            type={showPassword ? 'text' : 'password'} // Update input type based on showPassword
+            id="password"
+            className="password-input-field"
+            value={password}
+            onChange={this.onChangePassword}
+          />
+          <span
+            role="button"
+            tabIndex="0"
+            className={`eye-icon ${showPassword ? 'visible' : 'hidden'}`}
+            onClick={this.togglePasswordVisibility}
+          >
+            <FontAwesomeIcon icon={showPassword ? faLockOpen : faLock} />
+          </span>
+        </div>
+      </>
+    )
+  }
+
+  renderUsernameField = () => {
+    const {username} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="username-input-field"
+          value={username}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
+  }
+
+  render() {
+    const {showSubmitError, errorMsg} = this.state
+    const jwtToken = Cookies.get('jwt_token')
+    if (jwtToken !== undefined) {
+      return <Redirect to="/" />
+    }
+    return (
+      <div className="login-form-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          className="login-website-logo-mobile-image"
+          alt="website logo"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          className="login-image"
+          alt="website login"
+        />
+        <form className="form-container" onSubmit={this.submitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            className="login-website-logo-desktop-image"
+            alt="website logo"
+          />
+          <div className="input-container">{this.renderUsernameField()}</div>
+          <div className="input-container">{this.renderPasswordField()}</div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+          {showSubmitError && <p className="error-message">*{errorMsg}</p>}
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
